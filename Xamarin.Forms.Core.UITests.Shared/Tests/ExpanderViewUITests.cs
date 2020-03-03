6a1bd5fc31f71d5fc205f41b4412d285bbd0a137
Guido Neele <guido@crisisconnect.nl>
2020-03-03 11:43:03 +0100

    Added UI test for ExpanderView

---
+++ Xamarin.Forms.Core.UITests.Shared/Tests/ExpanderViewUITests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.Forms.Core.UITests
+{
+	[Category(UITestCategories.ExpanderView)]
+	internal class ExpanderViewUITests : BaseTestFixture
+	{
+		protected override void NavigateToGallery()
+		{
+			App.NavigateToGallery(GalleryQueries.ExpanderGallery);
+		}
+
+		[TestCase]
+		public void ExpanderView()
+		{
+			App.WaitForElement("The Second");
+			App.Tap("Expander Level 2");
+			App.WaitForElement("Hi, I am Red");
+			App.Tap("The Fourth");
+
+			App.WaitForNoElement("Hi, I am Red");
+
+			App.WaitForElement("Expander Level 2");
+			App.Tap("Expander Level 2");
+			App.WaitForElement("Hi, I am Red");
+			App.Tap("Expander Level 2");
+
+			App.WaitForNoElement("Hi, I am Red");
+
+			App.Tap("The Fourth");
+
+			App.WaitForNoElement("Expander Level 2");
+
+			App.Back();
+		}
+	}
+}
